fb4ea10e447944c6b1937d7589e34d963442edf8
jbuget@gmail.com <jbuget@gmail.com>
2020-03-10 01:20:54 +0100

    Add project structure
    - /controllers
    - /routes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,21 @@
-#!/usr/bin/env bash -e
+#!/bin/bash
+
+set -e
+
 git --version
 
-mkdir /app/tmp
-echo 'created dir /app/tmp'
-cd /app/tmp
-echo 'moved to dir /app/tmp'
+git config --global user.name "$GIT_USER_NAME"
+git config --global user.email "$GIT_USER_EMAIL"
+echo "Set Git global user information"
 
-git config --global user.name $GIT_USER_NAME
-git config --global user.email $GIT_USER_EMAIL
-echo 'set user.name: ' $(git config --global user.name)
-echo 'set user.email: ' $(git config --global user.email)
+mkdir "$DIR_SCRIPTS"/tmp
+cd "$DIR_SCRIPTS"/tmp
+echo "Created and moved to dir $DIR_SCRIPTS/tmp"
 
 git clone git@github.com:1024pix/test-deploy-from-slack.git
-echo 'cloned repo'
-
 cd test-deploy-from-slack
-echo 'moved to dir /app/tmp/test-deploy-from-slack'
+echo "Cloned and moved into repository"
 
 git commit --allow-empty -m 'TEST'
 git push origin master
-echo 'pushed commit'
+echo "pushed commit"
