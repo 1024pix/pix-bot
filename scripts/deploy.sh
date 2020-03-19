07ae78b0dcbe9eea528a7c81c90606d1d288be70
Jonas Grillères <jgrilleres@octo.com>
2020-03-19 12:02:21 +0100

    Init git repo in allowed diretory
    Scalingo only allows apps to write in /app/tmp or /tmp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 git --version
 
+cd /app/tmp
+git init
+
+git remote add git@github.com:1024pix/scalingo-app-manager.git
 git commit --allow-empty -m 'TEST'
 git push origin test-git-integration
