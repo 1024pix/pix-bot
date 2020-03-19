75254fce2e05b003f8f674fa4a9f54c883acc00a
Jonas Grillères <jgrilleres@octo.com>
2020-03-19 15:08:05 +0100

    Push empty to test repo from deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 git --version
 
+mkdir /app/tmp
 cd /app/tmp
-git init
 
-git remote add git@github.com:1024pix/scalingo-app-manager.git
+#git config --global user.name $GIT_USER_NAME
+#git config --global user.email $GIT_USER_EMAIL
+
+git clone git@github.com:1024pix/test-deploy-from-slack.git
+cd test-deploy-from-slack
+
 git commit --allow-empty -m 'TEST'
-git push origin test-git-integration
+git push origin master
