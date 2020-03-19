2c881c9f053567bfef562c43f6df1ac4ddee7f52
Jonas Grillères <jgrilleres@octo.com>
2020-03-19 16:16:00 +0100

    Generate key on one-off containers

---
+++ scripts/deploy-one-off.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir .ssh
+
+ssh-keyscan -H github.com > ~/.ssh/known_hosts
+
+echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+
+/app/scripts/deploy.sh
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@ git --version
 mkdir /app/tmp
 cd /app/tmp
 
-#git config --global user.name $GIT_USER_NAME
-#git config --global user.email $GIT_USER_EMAIL
-
+git config --global user.name $GIT_USER_NAME
+git config --global user.email $GIT_USER_EMAIL
 git clone git@github.com:1024pix/test-deploy-from-slack.git
+
 cd test-deploy-from-slack
 
 git commit --allow-empty -m 'TEST'
