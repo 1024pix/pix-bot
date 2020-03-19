719476ca7de45eb4955bdcf5d4e8957b6a9877e1
Jonas Grillères <jgrilleres@octo.com>
2020-03-19 17:32:47 +0100

    Debug deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 git --version
 
 mkdir /app/tmp
+echo 'created dir /app/tmp'
 cd /app/tmp
+echo 'moved to dir /app/tmp'
 
 git config --global user.name $GIT_USER_NAME
 git config --global user.email $GIT_USER_EMAIL
+echo 'set user.name: ' $(git config --global user.name)
+echo 'set user.email: ' $(git config --global user.email)
+
 git clone git@github.com:1024pix/test-deploy-from-slack.git
+echo 'cloned repo'
 
 cd test-deploy-from-slack
+echo 'moved to dir /app/tmp/test-deploy-from-slack'
 
 git commit --allow-empty -m 'TEST'
 git push origin master
+echo 'pushed commit'
