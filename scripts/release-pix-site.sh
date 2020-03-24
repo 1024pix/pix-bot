ee8bdbdb399607dc47b8988acd711268ef083334
Vincent Hardouin <vincent.hardouin@pix.fr>
2020-03-24 16:35:23 +0100

    Add new SSH_KEY for manage new repo

--- scripts/release-pix-site.sh
+++ scripts/release-pix-site.sh
@@ -8,21 +8,14 @@ VERSION_TYPE=$1
 
 echo "Version type ${VERSION_TYPE}"
 
-function install_ssh_key_for_git_operations {
-  mkdir -p ~/.ssh
-  ssh-keyscan -H github.com > ~/.ssh/known_hosts
-  echo "$SSH_KEY" | base64 -d > ~/.ssh/id_rsa
-  chmod 400 ~/.ssh/id_rsa
-}
-
 function clone_repository_and_move_inside {
   REPOSITORY_FOLDER=$(mktemp -d)
-  echo "Created temporary directory $REPOSITORY_FOLDER"
+  echo "Created temporary directory ${REPOSITORY_FOLDER}"
 
-  git clone git@github.com:1024pix/pix-site.git "$REPOSITORY_FOLDER"
+  git clone git@github.com:1024pix/pix-site.git "${REPOSITORY_FOLDER}"
   echo "Cloned repository to temporary directory"
 
-  cd "$REPOSITORY_FOLDER"
+  cd "${REPOSITORY_FOLDER}"
   echo "Moved to repository folder"
 }
 
@@ -42,18 +35,11 @@ function create_and_deploy_release {
   echo "Deploy new release"
 }
 
-function delete_repository_folder {
-  rm -rf "$REPOSITORY_FOLDER"
-  echo "Deleted temporary folder"
-}
-
 echo "Start deploying version ${VERSION_TYPE}…"
 
-install_ssh_key_for_git_operations
 clone_repository_and_move_inside
 configure_git_user_information
 install_required_packages
 create_and_deploy_release
-delete_repository_folder
 
 echo -e "Release deployment ${GREEN}succeeded${RESET_COLOR}."
