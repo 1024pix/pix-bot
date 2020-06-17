413639ed7731bb5b976d64b7151b494f4c20dbe9
Antoine Ceol <antoine.ceol@gmail.com>
2020-06-17 17:22:54 +0200

    Use correct variable for release

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,7 +35,7 @@ function update_production_branch {
 }
 
 function finalize_release_on_sentry {
-    npx sentry-cli releases -o pix finalize "${VERSION_NUMBER}"
+    npx sentry-cli releases -o pix finalize "${RELEASE_TAG}"
     echo "Finalized release on Sentry"
 }
 
