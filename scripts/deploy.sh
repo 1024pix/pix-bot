132f63ff10d2de4662e45702de2089cbb269c182
Guillaume Lagorce <guillaume@hey-gul.com>
2020-06-25 12:34:39 +0200

    Update the `deploy` script to deploy given release on given environment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,45 +6,58 @@ REPOSITORY_NAME=1024pix/pix
 source "$(dirname $0)"/common.sh
 
 RELEASE_TAG=$1
+ENVIRONMENT=$2
 
 echo "Version number ${RELEASE_TAG}"
 
+function get_target_branch_name {
+  case ${ENVIRONMENT} in
+    recette)
+      TARGET_BRANCH=master
+      ;;
+    production)
+      TARGET_BRANCH=prod
+      ;;
+    *)
+      echo -e "${RED} Environment shall be 'recette' or 'production'${RESET_COLOR}\n" >&2
+      exit 1
+      ;;
+  esac
+
+  echo "Get remote branch name: ${TARGET_BRANCH}"
+}
+
 function get_release_commit_hash {
-    local commit_hash
+  local commit_hash
 
-    commit_hash="$(git rev-parse --verify --quiet "${RELEASE_TAG}")"
+  commit_hash="$(git rev-parse --verify --quiet "${RELEASE_TAG}")"
 
-    if [ -z "${commit_hash}" ];
-    then
-        echo -e "${RED}Version ${RELEASE_TAG} does not exist!${RESET_COLOR}\n" >&2
-        exit 1
-    fi
+  if [ -z "${commit_hash}" ];
+  then
+      echo -e "${RED}Version ${RELEASE_TAG} does not exist!${RESET_COLOR}\n" >&2
+      exit 1
+  fi
 
-    echo "Fetched release commit hash"
+  echo "Fetched release commit hash"
 }
 
-function update_production_branch {
-    local annotated_version
-    local annotated_tag_hash
+function update_remote_branch {
+  local annotated_version
+  local annotated_tag_hash
 
-    annotated_version="${RELEASE_TAG}^{}"
-    annotated_tag_hash="$(git rev-parse --quiet "${annotated_version}")"
-    git push -f origin "$annotated_tag_hash":prod
+  annotated_version="${RELEASE_TAG}^{}"
+  annotated_tag_hash="$(git rev-parse --quiet "${annotated_version}")"
+  git push -f origin "$annotated_tag_hash":${TARGET_BRANCH}
 
-    echo "Pushed changes on branch origin/prod"
+  echo "Pushed release to branch origin/${TARGET_BRANCH}"
 }
 
-function finalize_release_on_sentry {
-    npx sentry-cli releases -o pix finalize "${RELEASE_TAG}"
-    echo "Finalized release on Sentry"
-}
+echo "Start deploying version ${RELEASE_TAG} to ${ENVIRONMENT}…"
 
-echo "Start deploying version ${RELEASE_TAG}…"
-
+get_target_branch_name
 clone_repository_and_move_inside
 configure_git_user_information
 get_release_commit_hash
-update_production_branch
-finalize_release_on_sentry
+update_remote_branch
 
 echo -e "Release deployment ${GREEN}succeeded${RESET_COLOR}."
