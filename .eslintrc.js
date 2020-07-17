2bf9779927465da8c55a9c04babd2e74f5d97db1
Benjamin Petetot <bpetetot@gmail.com>
2020-07-17 09:33:07 +0200

    Add Eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es2020': true,
+    'node': true,
+    'mocha': true,
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 11
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
