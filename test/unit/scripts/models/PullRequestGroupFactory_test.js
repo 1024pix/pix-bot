0abf26c540a283efab4ccdcca260298ef86d6d95
Christophe Charles <christophe.charles@octo.com>
2021-09-15 17:10:17 +0200

    Add PullRequestGroupFactory class to build the list of Pull Request Groups

---
+++ test/unit/scripts/models/PullRequestGroupFactory_test.js
@@ -0,0 +1,42 @@
+const { expect } = require('../../../test-helper');
+
+const PullRequestGroup = require('../../../../scripts/models/PullRequestGroup');
+const { Tag } = require('../../../../scripts/models/Tags');
+
+const PullRequestGroupFactory = require('../../../../scripts/models/PullRequestGroupFactory');
+
+describe('Unit | Script | Models | PullRequestGroupFactory', () => {
+
+  describe('#build', () => {
+
+    it('should return an array of Pull Request Groups', () => {
+      // given
+      const expectedPullRequestGroups = [
+        new PullRequestGroup({
+          tagToGrab: Tag.FEATURE,
+          groupTitle: '### :rocket: Enhancement',
+        }),
+        new PullRequestGroup({
+          tagToGrab: Tag.TECH,
+          groupTitle: '### :building_construction: Tech',
+        }),
+        new PullRequestGroup({
+          tagToGrab: Tag.BUGFIX,
+          groupTitle: '### :bug: Bug fix',
+        }),
+        new PullRequestGroup({
+          tagToGrab: Tag.OTHERS,
+          groupTitle: '### :coffee: Various',
+        }),
+      ];
+
+      // when
+      const result = PullRequestGroupFactory.build();
+
+      // then
+      expect(result).to.an('array').that.is.not.empty;
+      expect(result).to.deep.equal(expectedPullRequestGroups);
+    });
+  });
+
+});
