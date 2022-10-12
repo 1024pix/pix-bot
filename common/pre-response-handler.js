73837ce95c75e7d4af5cfe3a8ef647a681aca3b1
Pierre TOP (TOPI) <ptop@octo.com>
2022-10-12 12:06:54 +0200

    Trauncate logs

--- common/pre-response-handler.js
+++ common/pre-response-handler.js
@@ -1,8 +1,11 @@
+const { scalingo } = require('../config');
+
 function handleErrors(request, h) {
   const response = request.response;
 
   if (response instanceof Error && response.output.statusCode !== 401 && response.output.statusCode !== 400) {
-    console.log(response.stack);
+    const stacktrace = response.stack.slice(0, scalingo.maxLogLength);
+    console.error(stacktrace);
     return h.response('An error occurred, please try again later').code(500);
   }
 
