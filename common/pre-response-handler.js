73ce5f2b3ac9415da7aa97e71e6365fb5695537a
Pierre TOP (TOPI) <ptop@octo.com>
2022-10-12 10:56:52 +0200

    Do not catch expected errors

--- common/pre-response-handler.js
+++ common/pre-response-handler.js
@@ -1,7 +1,7 @@
 function handleErrors(request, h) {
   const response = request.response;
 
-  if (response instanceof Error) {
+  if (response instanceof Error && response.output.statusCode !== 401 && response.output.statusCode !== 400) {
     console.log(response.stack);
     return h.response('An error occurred, please try again later').code(500);
   }
