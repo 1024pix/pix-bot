4fd1f81f0df52cf6fc1572aadcdc78917d000605
Yoan de LUCA <yoan.de-luca@pix.fr>
2022-12-02 17:22:31 +0100

    Explain why application name is incorrect

--- common/models/ScalingoAppName.js
+++ common/models/ScalingoAppName.js
@@ -1,12 +1,14 @@
 //TODO use EnvVars
 const config = require('../../config');
 const alphanumericAndDashOnly = /^([a-zA-Z0-9]+-)+[a-zA-Z0-9]+$/;
-const prefix = 'pix-';
+const prefix = config.scalingo.validAppPrefix;
 class ScalingoAppName {
   static isApplicationNameValid(applicationName) {
     const suffix = config.scalingo.validAppSuffix;
     const appNameMatchesRegex = applicationName.search(alphanumericAndDashOnly) >= 0;
-    const appNameHasCorrectLength = applicationName.length >= 6 && applicationName.length <= 46;
+    const appNameHasCorrectLength =
+      applicationName.length >= config.scalingo.validAppNbCharMin &&
+      applicationName.length <= config.scalingo.validAppNbCharMax;
     const appNameStartsWithPix = applicationName.startsWith(prefix);
     const appNameEndsWithCorrectSuffix = suffix.includes(applicationName.split('-').slice(-1)[0]);
     return appNameMatchesRegex && appNameHasCorrectLength && appNameStartsWithPix && appNameEndsWithCorrectSuffix;
---
+++ test/integration/common/services/slack/view-submissions_test.js
@@ -0,0 +1,130 @@
+const { expect, sinon } = require('../../../../test-helper');
+const viewSubmissions = require('../../../../../run/services/slack/view-submissions');
+const slackGetUserInfos = require('../../../../../common/services/slack/surfaces/user-infos/get-user-infos');
+
+describe('view-submissions', function () {
+  describe('#submitApplicationNameSelection', function () {
+    context('when application name is invalid', function () {
+      it('should return error message', async function () {
+        // given
+        const applicationName = 'foo-bar-production';
+        const applicationEnvironment = 'production';
+        const applicationEnvironmentName = 'production';
+        const userId = 1;
+        const userEmail = 'john.doe@pix.fr';
+
+        const getUserEmailStub = sinon.stub(slackGetUserInfos, 'getUserEmail');
+        getUserEmailStub.resolves(userEmail);
+
+        const payload = {
+          view: {
+            state: {
+              values: {
+                'create-app-name': {
+                  'scalingo-app-name': {
+                    value: applicationName,
+                  },
+                },
+                'application-env': {
+                  item: {
+                    selected_option: {
+                      value: applicationEnvironment,
+                      text: {
+                        text: applicationEnvironmentName,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+          user: { id: userId },
+        };
+
+        // when
+        const actual = await viewSubmissions.submitApplicationNameSelection(payload);
+
+        // then
+        expect(actual).to.deep.equal({
+          response_action: 'errors',
+          errors: {
+            'create-app-name': `foo-bar-production is incorrect, it should start with "pix-" and end with one of the following : production,review,integration,recette,sandbox,dev,router,test. Also the length should be between 6 and 46 characters.`,
+          },
+        });
+      });
+    });
+    context('when application name is valid', function () {
+      it('should return response', async function () {
+        // given
+        const applicationName = 'pix-foo-production';
+        const applicationEnvironment = 'production';
+        const applicationEnvironmentName = 'production';
+        const userId = 1;
+        const userEmail = 'john.doe@pix.fr';
+
+        const getUserEmailStub = sinon.stub(slackGetUserInfos, 'getUserEmail');
+        getUserEmailStub.resolves(userEmail);
+
+        const payload = {
+          view: {
+            state: {
+              values: {
+                'create-app-name': {
+                  'scalingo-app-name': {
+                    value: applicationName,
+                  },
+                },
+                'application-env': {
+                  item: {
+                    selected_option: {
+                      value: applicationEnvironment,
+                      text: {
+                        text: applicationEnvironmentName,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+          user: { id: userId },
+        };
+
+        // when
+        const actual = await viewSubmissions.submitApplicationNameSelection(payload);
+
+        // then
+        expect(actual).to.deep.equal({
+          response_action: 'push',
+          view: {
+            blocks: [
+              {
+                text: {
+                  text: "Vous vous apprêtez à créer l'application *pix-foo-production* dans la région : *production* et à inviter cet adresse email en tant que collaborateur : *john.doe@pix.fr*",
+                  type: 'mrkdwn',
+                },
+                type: 'section',
+              },
+            ],
+            callback_id: 'application-creation-confirmation',
+            close: {
+              text: 'Annuler',
+              type: 'plain_text',
+            },
+            private_metadata:
+              '{"applicationName":"pix-foo-production","applicationEnvironment":"production","userEmail":"john.doe@pix.fr"}',
+            submit: {
+              text: '🚀 Go !',
+              type: 'plain_text',
+            },
+            title: {
+              text: 'Confirmation',
+              type: 'plain_text',
+            },
+            type: 'modal',
+          },
+        });
+      });
+    });
+  });
+});
