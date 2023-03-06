0e151a31142ff025ea24ec8fa5efd90937f21628
François de Metz <francois.de-metz@pix.fr>
2023-03-06 15:56:43 +0100

    Move pull request handling to a function

--- build/controllers/github.js
+++ build/controllers/github.js
@@ -51,6 +51,40 @@ const addMessageToPullRequest = async ({ repositoryName, pullRequestId, scalingo
   });
 };
 
+async function pullRequestWebhook(request) {
+  const payload = request.payload;
+  const repository = payload.pull_request.head.repo.name;
+  const prId = payload.number;
+  const reviewApps = repositoryToScalingoAppsReview[repository];
+  const labelsList = payload.pull_request.labels;
+  if (payload.pull_request.head.repo.fork) {
+    return 'No RA for a fork';
+  }
+  if (payload.action !== 'opened') {
+    return `Ignoring ${payload.action} action`;
+  }
+  if (!reviewApps) {
+    return 'No RA configured for this repository';
+  }
+  if (labelsList.some((label) => label.name == 'no-review-app')) {
+    return 'RA disabled for this PR';
+  }
+  try {
+    const client = await ScalingoClient.getInstance('reviewApps');
+    for (const appName of reviewApps) {
+      await client.deployReviewApp(appName, prId);
+    }
+    await addMessageToPullRequest({
+      repositoryName: repository,
+      scalingoReviewApps: reviewApps,
+      pullRequestId: prId,
+    });
+    return `Created RA on app ${reviewApps.join(', ')} with pr ${prId}`;
+  } catch (error) {
+    throw new Error(`Scalingo APIError: ${error.message}`);
+  }
+}
+
 module.exports = {
   getMessageTemplate,
   getMessage,
@@ -58,37 +92,7 @@ module.exports = {
   async processWebhook(request) {
     const eventName = request.headers['x-github-event'];
     if (eventName === 'pull_request') {
-      const payload = request.payload;
-      const repository = payload.pull_request.head.repo.name;
-      const prId = payload.number;
-      const reviewApps = repositoryToScalingoAppsReview[repository];
-      const labelsList = payload.pull_request.labels;
-      if (payload.pull_request.head.repo.fork) {
-        return 'No RA for a fork';
-      }
-      if (payload.action !== 'opened') {
-        return `Ignoring ${payload.action} action`;
-      }
-      if (!reviewApps) {
-        return 'No RA configured for this repository';
-      }
-      if (labelsList.some((label) => label.name == 'no-review-app')) {
-        return 'RA disabled for this PR';
-      }
-      try {
-        const client = await ScalingoClient.getInstance('reviewApps');
-        for (const appName of reviewApps) {
-          await client.deployReviewApp(appName, prId);
-        }
-        await addMessageToPullRequest({
-          repositoryName: repository,
-          scalingoReviewApps: reviewApps,
-          pullRequestId: prId,
-        });
-        return `Created RA on app ${reviewApps.join(', ')} with pr ${prId}`;
-      } catch (error) {
-        throw new Error(`Scalingo APIError: ${error.message}`);
-      }
+      return pullRequestWebhook(request);
     } else {
       return `Ignoring ${eventName} event`;
     }
