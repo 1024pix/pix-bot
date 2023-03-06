2e6ac7d2514744409fa5693d00de2faeed1d4d8d
François de Metz <francois.de-metz@pix.fr>
2023-03-06 14:50:37 +0100

    Use the config

--- build/services/eco-mode-service.js
+++ build/services/eco-mode-service.js
@@ -5,18 +5,18 @@ module.exports = {
   async start() {
     const scalingoToken = config.scalingo.reviewApps.token;
     const scalingoApiUrl = config.scalingo.reviewApps.apiUrl;
-    const stopCronTime = process.env.REVIEW_APP_STOP_SCHEDULE;
-    const restartCronTime = process.env.REVIEW_APP_START_SCHEDULE;
+    const stopCronTime = config.ecoMode.stopSchedule;
+    const startCronTime = config.ecoMode.startSchedule;
     const timeZone = process.env.TIME_ZONE || 'Europe/Paris';
     const ignoredReviewApps = process.env.IGNORED_REVIEW_APPS ? process.env.IGNORED_REVIEW_APPS.split(',') : [];
 
-    if (!config.ecoMode.stopSchedule || !config.ecoMode.startSchedule) {
+    if (!stopCronTime || !startCronTime) {
       console.log('No schedule configured for eco mode - skipping.');
       return;
     }
     const reviewAppManager = new ReviewAppManager(scalingoToken, scalingoApiUrl, {
       stopCronTime,
-      restartCronTime,
+      restartCronTime: startCronTime,
       timeZone,
       ignoredReviewApps,
     });
