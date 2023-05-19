77ddd3f8a9a168bd48b9d785e1be870bfcfc582a
Emeric Martineau <11473190+emeric-martineau@users.noreply.github.com>
2023-05-19 14:34:21 +0200

    [FEATURE] Les démarrage de job ne sont pas tracés dans Pix bot (PIX-7053)

--- common/services/cron-job.js
+++ common/services/cron-job.js
@@ -1,17 +1,49 @@
 const CronJob = require('cron').CronJob;
 const parisTimezone = 'Europe/Paris';
+const logger = require('./logger');
 
 function createCronJob(jobName, jobFunction, jobSchedule) {
   if (!jobSchedule) {
-    console.log(`No schedule configured for cron job ${jobName} - skipping.`);
+    logger.warn({
+      event: 'cron',
+      message: `No schedule configured for cron job ${jobName} - skipping.`,
+    });
+
     return;
   }
   try {
-    new CronJob(jobSchedule, jobFunction, null, true, parisTimezone);
+    new CronJob(
+      jobSchedule,
+      async () => {
+        logger.info({
+          event: 'cron',
+          message: `Start job '${jobName}'`,
+        });
+
+        try {
+          await jobFunction();
+        } finally {
+          logger.info({
+            event: 'cron',
+            message: `End job '${jobName}'`,
+          });
+        }
+      },
+      null,
+      true,
+      parisTimezone
+    );
   } catch (e) {
-    console.error(e);
+    logger.error({
+      event: 'cron',
+      message: e,
+    });
   }
-  console.log(`Started job ${jobName} with cron time "${jobSchedule}"`);
+
+  logger.info({
+    event: 'cron',
+    message: `Started job ${jobName} with cron time "${jobSchedule}"`,
+  });
 }
 
 module.exports = {
--- common/services/logger.js
+++ common/services/logger.js
@@ -6,7 +6,12 @@ const info = (message, injectedLogger = console) => {
   injectedLogger.log(JSON.stringify(message));
 };
 
+const warn = (message, injectedLogger = console) => {
+  injectedLogger.warn(JSON.stringify(message));
+};
+
 module.exports = {
   error,
   info,
+  warn,
 };
