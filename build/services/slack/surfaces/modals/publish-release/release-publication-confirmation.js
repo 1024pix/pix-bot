3ff7dd444fb53a3b8377e46fdc5396e850670f0e
Mickaël Alibert <mickael.alibert@pix.fr>
2023-09-13 18:17:45 +0200

    :bug: Fix link display in release-publication-confirmation view

--- build/services/slack/surfaces/modals/publish-release/release-publication-confirmation.js
+++ build/services/slack/surfaces/modals/publish-release/release-publication-confirmation.js
@@ -13,7 +13,7 @@ function _createModal({ releaseType, hasConfigFileChanged, latestTag }) {
     ...(hasConfigFileChanged
       ? [
           Blocks.Section({
-            text: `:warning: Il y a eu des ajout(s)/suppression(s) dans le fichier [config.js](https://github.com/1024pix/pix/compare/${latestTag}...dev). Pensez à vérifier que toutes les variables d'environnement sont bien à jour sur *Scalingo RECETTE*.`,
+            text: `:warning: Il y a eu des ajout(s)/suppression(s) dans le fichier <https://github.com/1024pix/pix/compare/${latestTag}...dev|*config.js*>. Pensez à vérifier que toutes les variables d'environnement sont bien à jour sur *Scalingo RECETTE*.`,
           }),
         ]
       : []),
