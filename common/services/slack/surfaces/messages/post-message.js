80386b3566c4e3d425fa8511701dcd8bdd9b8382
Mickaël Alibert <mickael.alibert@pix.fr>
2023-09-16 11:34:46 +0200

    :loud_sound: Log slack API errors

--- common/services/slack/surfaces/messages/post-message.js
+++ common/services/slack/surfaces/messages/post-message.js
@@ -1,5 +1,6 @@
 const config = require('../../../../../config');
 const { httpAgent } = require('../../../../http-agent');
+const logger = require('../../../logger');
 
 module.exports = {
   async postMessage({ message, attachments, channel = '#tech-releases', injectedHttpAgent = httpAgent }) {
@@ -15,6 +16,15 @@ module.exports = {
       attachments: attachments,
     };
 
-    await injectedHttpAgent.post({ url, payload, headers });
+    const slackResponse = await injectedHttpAgent.post({ url, payload, headers });
+    if (slackResponse.isSuccessful) {
+      if (!slackResponse.data.ok) {
+        logger.error({
+          event: 'slack-post-message',
+          message: `Slack error occured while sending message : ${slackResponse.data.error}`,
+          stack: `Payload for error was ${JSON.stringify(payload)}`,
+        });
+      }
+    }
   },
 };
