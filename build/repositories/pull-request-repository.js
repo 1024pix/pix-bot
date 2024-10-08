c3d13d529294fd36160cdeaf6019d27e7092a4a7
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-10-08 16:57:00 +0200

    feat: add merge-queue
    Co-authored-by: Yannick François <yannick.francois@pix.fr>

--- build/repositories/pull-request-repository.js
+++ build/repositories/pull-request-repository.js
@@ -1,9 +1,19 @@
-import {knex} from '../../db/knex-database-connection.js';
+import { knex } from '../../db/knex-database-connection.js';
 
 async function save({ number, repositoryName }) {
   return knex('pull_requests').insert({ number, repositoryName });
 }
 
-export {
-  save
+async function isAtLeastOneMergeInProgress() {
+  const isAtLeastOneMergeInProgress = await knex('pull_requests')
+    .select('isCurrentlyMerge')
+    .where({ isCurrentlyMerge: true })
+    .first();
+  return Boolean(isAtLeastOneMergeInProgress);
 }
+
+async function update({ number, repositoryName, isCurrentlyMerge }) {
+  await knex('pull_requests').where({ number, repositoryName }).update({ isCurrentlyMerge });
+}
+
+export { save, isAtLeastOneMergeInProgress, update };
--- test/integration/build/repositories/pull-request-repository_test.js
+++ test/integration/build/repositories/pull-request-repository_test.js
@@ -3,7 +3,11 @@ import { knex } from '../../../../db/knex-database-connection.js';
 import { expect } from '../../../test-helper.js';
 
 describe('PullRequestRepository', function () {
-  context('#save', function () {
+  beforeEach(async function () {
+    await knex('pull_requests').delete();
+  });
+
+  describe('#save', function () {
     it('stores pull request to be merged', async function () {
       await pullRequestRepository.save({ number: 123, repositoryName: 'pix-sample-repo' });
 
@@ -15,7 +19,42 @@ describe('PullRequestRepository', function () {
       expect(result).to.deep.equal({
         number: 123,
         repositoryName: 'pix-sample-repo',
+        isCurrentlyMerging: false,
       });
+    });
+  });
+
+  describe('#isAtLeastOneMergeInProgress', function () {
+    context('when there is at least one pr in merging', function () {
+      it('should return true', async function () {
+        await knex('pull_requests').insert({ number: 123, repositoryName: 'pix-sample-repo', isCurrentlyMerge: true });
+
+        const isCurrentlyMerging = await pullRequestRepository.isAtLeastOneMergeInProgress();
+
+        expect(isCurrentlyMerging).to.be.true;
+      });
+    });
+
+    context('when no pr are currently in progress', function () {
+      it('should return false', async function () {
+        await knex('pull_requests').insert({ number: 123, repositoryName: 'pix-sample-repo', isCurrentlyMerge: false });
+
+        const isCurrentlyMerging = await pullRequestRepository.isAtLeastOneMergeInProgress();
+
+        expect(isCurrentlyMerging).to.be.false;
+      });
+    });
+  });
+
+  describe('#update', function () {
+    it('should update only the given pull_request', async function () {
+      await knex('pull_requests').insert({ number: 123, repositoryName: 'pix-sample-repo', isCurrentlyMerge: false });
+      await knex('pull_requests').insert({ number: 1234, repositoryName: 'pix-sample-repo', isCurrentlyMerge: false });
+
+      await pullRequestRepository.update({ number: 1234, repositoryName: 'pix-sample-repo', isCurrentlyMerge: true });
+
+      const results = await knex('pull_requests').where({ isCurrentlyMerge: true });
+      expect(results).to.have.been.lengthOf(1);
     });
   });
 });
