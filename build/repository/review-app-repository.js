b931a1014a6026253b7733c31160db161bd7f401
Mathieu Gilet <mathieu.gilet@pix.fr>
2024-11-14 15:44:48 +0100

    fix: merge review-app rather than throw an error when the RA is recreated

--- build/repository/review-app-repository.js
+++ build/repository/review-app-repository.js
@@ -1,7 +1,10 @@
 import { knex } from '../../db/knex-database-connection.js';
 
 export const create = async function ({ name, repository, prNumber, parentApp }) {
-  await knex('review-apps').insert({ name, repository, prNumber, parentApp });
+  await knex('review-apps').insert({ name, repository, prNumber, parentApp }).onConflict('name').merge({
+    createdAt: new Date(),
+    isDeployed: false,
+  });
 };
 
 export const markAsDeployed = async function ({ name }) {
