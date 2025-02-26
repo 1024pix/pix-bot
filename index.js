28e8074c363bbcbecf48f2468b9bc40bdf0fc8ec
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2025-02-26 17:32:43 +0100

    refactor: remove createCronJob in favor of taskScheduler

--- index.js
+++ index.js
@@ -2,29 +2,15 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import ecoModeService from './build/services/eco-mode-service.js';
-import { createCronJob } from './common/services/cron-job.js';
-import github from './common/services/github.js';
 import { logger } from './common/services/logger.js';
-import { config } from './config.js';
-import { deploy } from './run/services/deploy.js';
 import { taskScheduler } from './run/services/task-scheduler.js';
-import tasks from './run/services/tasks.js';
+import runTasks from './run/services/tasks.js';
 import server from './server.js';
 
 const init = async () => {
   await ecoModeService.start();
 
-  createCronJob(
-    'Deploy Pix site',
-    async () => {
-      const repoName = config.PIX_SITE_REPO_NAME;
-      const releaseTag = await github.getLatestReleaseTag(repoName);
-      deploy(repoName, config.PIX_SITE_APPS, releaseTag);
-    },
-    config.pixSiteDeploy.schedule,
-  );
-
-  taskScheduler(tasks);
+  taskScheduler(runTasks);
 
   await server.start();
 
