b0fda4696399bccef0d4fb55c97a4512b1b1d0c2
Mathieu Gilet <mathieu.gilet@pix.fr>
2025-03-14 15:40:22 +0100

    tech: add pix-exploit-front-review

--- build/controllers/github.js
+++ build/controllers/github.js
@@ -18,7 +18,7 @@ const repositoryToScalingoAppsReview = {
   'pix-db-stats': ['pix-db-stats-review'],
   'pix-editor': ['pix-lcms-review'],
   'pix-epreuves': ['pix-epreuves-review'],
-  'pix-exploit': ['pix-exploit-review'],
+  'pix-exploit': ['pix-exploit-api-review', 'pix-exploit-front-review'],
   'pix-site': ['pix-site-review', 'pix-pro-review'],
   'pix-tutos': ['pix-tutos-review'],
   'pix-ui': ['pix-ui-review'],
