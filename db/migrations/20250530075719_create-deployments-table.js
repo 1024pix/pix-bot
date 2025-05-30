07c34b6ef0a2deab2a69782c635ec510726ec293
Theotime2005 <theotime.berthod@pix.fr>
2025-05-30 10:09:45 +0200

    feat: add a migration to create table

---
+++ db/migrations/20250530075719_create-deployments-table.js
@@ -0,0 +1,24 @@
+const TABLE_NAME = 'deployments';
+const APPLICATIONS = ['pix-app', 'pix-orga', 'pix-admin', 'pix-certif', 'pix-junior', 'audit-logger'];
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  return await knex.schema.createTable(TABLE_NAME, function (table) {
+    table.string('tag').comment('Deployment tag, e.g., v1.0.0').primary();
+    APPLICATIONS.map((application) => {
+      table.boolean(application).defaultTo(false).comment(`The deployment of ${application}`);
+    });
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  return await knex.schema.dropTable(TABLE_NAME);
+};
+
+export { up, down };
