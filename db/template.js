3cd26ee31829eba43d1b553d1c60a48483450853
Theotime2005 <theotime.berthod@pix.fr>
2025-06-03 16:01:54 +0200

    feat: add a template for migration

---
+++ db/template.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  // Complete with your migration here.
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  // Complete with your down migration here.
+};
+
+export { up, down };
