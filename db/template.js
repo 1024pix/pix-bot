b0fa6444b2aed816a2de0e8ca032a73b0e1919d6
Theotime2005 <theotime.berthod@pix.fr>
2025-06-03 16:01:54 +0200

    feat: add a template for migration

---
+++ db/template.js
@@ -0,0 +1,25 @@
+const TABLE_NAME = 'my_table';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = async function (knex) {
+  await knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.integer('age').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = async function (knex) {
+  await knex.schema.dropTable(TABLE_NAME);
+};
+
+export { up, down };
