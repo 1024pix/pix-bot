3bbdd21dcf8df82ff8fb81c0b54aa9300ef8283e
Theotime2005 <theotime.berthod@pix.fr>
2025-06-10 09:18:18 +0200

    feat: add repository to manage table

---
+++ common/repositories/applications-deployment.repository.js
@@ -0,0 +1,35 @@
+import { knex } from '../../db/knex-database-connection.js';
+import { config } from '../../config.js';
+
+const TABLE_NAME = 'applications_deployments';
+
+async function createVersion({ version, environment }) {
+  for (const application of config.PIX_APPS) {
+    await knex(TABLE_NAME).insert({
+      version,
+      environment,
+      'app-name': application,
+    });
+  }
+}
+
+async function markHasDeployed({ version, environment, app }) {
+  return await knex(TABLE_NAME)
+    .where({ version, 'app-name': app, environment })
+    .update({ 'is-deployed': true, 'deployed-at': new Date() });
+}
+
+async function getByVersionAndEnvironment({ version, environment }) {
+  const rows = await knex(TABLE_NAME).where({ version, environment }).select('app-name', 'is-deployed', 'deployed-at');
+  return rows.map((row) => ({
+    app: row['app-name'],
+    isDeployed: row['is-deployed'],
+    deployedAt: row['deployed-at'],
+  }));
+}
+
+async function removeByVersionAndEnvironment({ version, environment }) {
+  return await knex(TABLE_NAME).where({ version, environment }).del();
+}
+
+export { createVersion, markHasDeployed, getByVersionAndEnvironment, removeByVersionAndEnvironment };
